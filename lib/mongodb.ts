1832367f695a94809401f6825d6e4ce86dd2407c
Arunlal.softius <arunlalpp.softius@gmail.com>
2026-06-30 13:01:35 +0530

    Fix MONGODB_URI check to run at runtime not build time

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,16 +1,15 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI!
-
-if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined')
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const cached = (global as any).__mongoose ?? ((global as any).__mongoose = {})
 
 export async function connectDB() {
+    const uri = process.env.MONGODB_URI
+    if (!uri) throw new Error('MONGODB_URI is not defined')
+
     if (cached.conn) return cached.conn
     if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false })
+        cached.promise = mongoose.connect(uri, { bufferCommands: false })
     }
     cached.conn = await cached.promise
     return cached.conn
